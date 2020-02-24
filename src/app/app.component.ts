a3884f03f167a0b57c88bca1bd5e521229a83ea7
Myron Young <myronyoung@Myrons-MacBook-Pro.local>
2020-02-24 18:32:23 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, Event, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'capway-banking';
+
+
+
+  constructor(private router: Router){
+
+    this.router.events.subscribe( (v: Event) => {
+     console.log(v);
+   });
+ }
+ ngOnInit() {
+
+  }
+
+  onRouterOutletActivate() {
+
+  }
+
+  onRouterOutletDeactivate() {
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms'
 
 import { AppComponent } from './app.component';
 import { NavigationMenuComponent } from './navigation-menu/navigation-menu.component';
@@ -7,11 +10,11 @@ import { SubMenuAccountComponent } from './sub-menu-account/sub-menu-account.com
 import { SubMenuSendandpayComponent } from './sub-menu-sendandpay/sub-menu-sendandpay.component';
 import { FooterComponent } from './footer/footer.component';
 import { DownloadComponent } from './download/download.component';
-import { SecurityComponent } from './security/security.component';
 import { SubMenuSecurityComponent } from './sub-menu-security/sub-menu-security.component';
 import { AccountOverviewComponent } from './account-overview/account-overview.component';
 import { AccountTransactionsComponent } from './account-transactions/account-transactions.component';
 import { AccountGoalsComponent } from './account-goals/account-goals.component';
+import { AccountStatementsComponent } from './account-statements/account-statements.component';
 import { AccountSecurityComponent } from './account-security/account-security.component';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
 import { SendandpaySendComponent } from './sendandpay-send/sendandpay-send.component';
@@ -24,6 +27,7 @@ import { SecurityDisputeComponent } from './security-dispute/security-dispute.co
 import { SecurityAccountsecurityComponent } from './security-accountsecurity/security-accountsecurity.component';
 import { SecurityDisputehistoryComponent } from './security-disputehistory/security-disputehistory.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -32,11 +36,11 @@ import { SecurityDisputehistoryComponent } from './security-disputehistory/secur
     SubMenuSendandpayComponent,
     FooterComponent,
     DownloadComponent,
-    SecurityComponent,
     SubMenuSecurityComponent,
     AccountOverviewComponent,
     AccountTransactionsComponent,
     AccountGoalsComponent,
+    AccountStatementsComponent,
     AccountSecurityComponent,
     AccountSettingsComponent,
     SendandpaySendComponent,
@@ -50,8 +54,41 @@ import { SecurityDisputehistoryComponent } from './security-disputehistory/secur
     SecurityDisputehistoryComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path:'account',
+        component: SubMenuAccountComponent,
+        children:[
+          { path: 'transactions', component: AccountTransactionsComponent },
+          { path: 'goals', component: AccountGoalsComponent },
+          { path:'statements', component: AccountStatementsComponent },
+          { path:'security', component: AccountSecurityComponent },
+          { path:'settings', component: AccountSettingsComponent },
+        ]
+      },
+      { path:'sendandpay',
+      component: SubMenuSendandpayComponent,
+        children:[
+          { path:'send', component: SendandpaySendComponent },
+          { path:'request', component: SendandpayRequestComponent },
+          { path:'billpay', component: SendandpayBillpayComponent },
+          { path:'transferhistory', component: SendandpayTransferhistoryComponent },
+        ]
+      },
+      { path:'security',
+      component: SubMenuSecurityComponent,
+        children:[
+          { path:'cardsecurity', component: SecurityCardsecurityComponent },
+          { path:'dispute', component: SecurityDisputeComponent },
+          { path:'accountsecurity', component: SecurityAccountsecurityComponent }
+        ]
+      },
+  ],{
+    enableTracing: true
+  }),
+],
   providers: [],
   bootstrap: [AppComponent]
 })
